ca0bd891c115553dee2d2a2bbc55d9987c5e44a6
foak77 <emailtofred@gmail.com>
2020-09-01 17:25:02 -0400

    nodemailer update config.env / email

--- utils/email.js
+++ utils/email.js
@@ -2,18 +2,41 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   // 1) Create a transporter
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.EMAIL_HOST,
+  //   port: process.env.EMAIL_PORT,
+  //   auth: {
+  //     user: process.env.EMAIL_USERNAME,
+  //     pass: process.env.EMAIL_PASSWORD,
+  //   },
+  // });
+
+  let xUser = process.env.EMAIL_USERNAME;
+  let xPass = process.env.EMAIL_PASSWORD;
+  let xPort = process.env.EMAIL_PORT;
+  let xHost = process.env.EMAIL_HOST;
+
+  // console.log(process.env.EMAIL_USERNAME);
+  // console.log(process.env.EMAIL_PASSWORD);
+  // console.log(process.env.EMAIL_HOST);
+  // console.log(process.env.EMAIL_PORT);
+
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    host: xHost,
+    // host: 'smtp.mailtrap.io',
+    port: xPort,
+    // port: 2525,
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: xUser,
+      // user: '52b5f2b9fa0ab0',
+      pass: xPass,
+      // pass: '802145b6860e6e',
     },
   });
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Fred Oak<fred@fred.com>',
+    from: 'Fred<fred@fred.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
